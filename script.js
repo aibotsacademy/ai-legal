9b8002b82d6056beea4c09fe6c306d3ebf5d2e62
Uriel Badillo Garrido <urielbadillogarrido@MacBook-Air-de-Uriel.local>
2024-07-25 19:22:04 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const text = "Bienvenido a aiTuring";
+    const typewriterElement = document.getElementById('typewriter');
+    let i = 0;
+    function typeWriter() {
+        if (i < text.length) {
+            typewriterElement.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, 150); // Ajusta el tiempo para cambiar la velocidad de 'escritura'
+        }
+    }
+    typeWriter();
+});
